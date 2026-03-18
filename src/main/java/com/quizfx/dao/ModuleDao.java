176708055612fb68a4f82ffb0ca90aeb132203d8
fatimazahrahenoud <fatimazahrahenoud@gmail.com>
2026-03-18 17:28:16 +0100

    ajout des fxmls

--- src/main/java/com/quizfx/dao/ModuleDao.java
+++ src/main/java/com/quizfx/dao/ModuleDao.java
@@ -1,4 +1,26 @@
-package com.quizfx.dao;
 
-public class ModuleDao {
+
+class ModuleDao {
+
+    private int id;
+    private String titre;
+
+    public ModuleDao() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 }
